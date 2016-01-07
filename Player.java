12e289764c32392b40ba211c1777b7cabd6d6bff
Kevin Zhang <kevinzhang3702@gmail.com>
2016-01-07 12:16:32 -0500

    Started Player.java

---
+++ Player.java
@@ -0,0 +1,10 @@
+public class Player{
+    private int money;
+    private ArrayList<Integer> properties;//int array because properties will be associated with a number. 
+
+    public Player(){
+	money = 1500;
+	
+    }
+
+}
