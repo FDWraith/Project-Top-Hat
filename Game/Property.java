210014b6c60fef9f0103e997e2fdd50e9332a861
Kevin Zhang <kevinzhang3702@gmail.com>
2016-01-08 12:20:37 -0500

    Added Accessor Methods to Property.java

--- Game/Property.java
+++ Game/Property.java
@@ -22,8 +22,56 @@ public class Property extends Slot{
 	colorGroup = colorG;
     }
 
+    //Accessors 
+    
+
+    /**Use this to get the price of the unowned property
+     *@return price of property
+     */
+    public int getBuyPrice(){
+	return buyPrice;
+    }
+
+    /**Use this to get the price that needs to be paid if you land on this owned property.
+     *@return renting fee
+     */
+    public int getRentPrice(){
+	return rentPrice;
+    }
+
+    /**Use this to get the price of a house.
+     *@return the cost of a single house.
+     */
+    public int getHousePrice(){
+	return housePrice;
+    }
+
+    /**Use this to get the price of a hotel.
+     *@return the cost of a single hotel.
+     */
+    public int getHotelPrice(){
+	return hotelPrice;
+    }
+    
+    /**Use this to get the mortgage value of this property.
+     *@return the mortgage value.
+     */
+    public int getMortgage(){
+	return mortgageValue;
+    }
+
+    /**Use this to figure out which colorGroup that this property belongs to.
+     *@return a String that represents the colorGroup.
+     */
+    public String getColorGroup(){
+	return colorGroup;
+    }
+
 
     //Mandatory method.
+    
+    /**This is what happens when you land on the property. 
+     */
     public void doAction(){
     }
 
