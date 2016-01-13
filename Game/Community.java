861e042502f489a18730fc410832d3c4c6a4f655
Kevin Zhang <kevinzhang3702@gmail.com>
2016-01-13 12:08:50 -0500

    Added Chance and Community.java

---
+++ Game/Community.java
@@ -0,0 +1,12 @@
+public class Community extends Slot{
+    
+    public Community(int ID){
+	setLocation(ID);
+    }
+
+    //Mandatory Action
+    public void doAction(){
+	//fill in this later
+    }
+   
+}
